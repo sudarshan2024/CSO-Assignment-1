f912df13a1da32fc43cec5982025f3d28648f74d
sudarshan2024 <sudarshannikhil25@gmail.com>
2024-03-14 08:56:54 +0530

    q5 completed

--- q5/q5.s
+++ q5/q5.s
@@ -4,26 +4,28 @@
 product:
     movq $1, %rax # Product of array elements
     movq $0, %rbx # rbx = 0 (Loop variable i)
-    movq $0, %rcx # rcx = 0 (If 0 in a)
+    movq $0, %rcx # rcx = 0 (Number of 0s in a)
     jmp .LoopCondition
 
 .Loop:
     movq (%rdi, %rbx, 8), %r8 # r8 = a[i]
     cmpq $0, %r8 # If 0 in a
-    jz .BackToLoop # continue if a[i] = 00
+    jz .BackToLoop # continue if a[i] = 0
     imulq %r8, %rax # rax *= a[i]
     incq %rbx # i++
     jmp .LoopCondition
 
 .BackToLoop:
     incq %rcx # rcx++
+    cmpq $2, %rcx
+    jz .TwoZeroesCondition
     incq %rbx
     jmp .LoopCondition
     
 .LoopCondition:
     cmpq %rsi, %rbx # Compare i and n
     jb .Loop # Jump to loop if i < n
-    movq $0, %rbx # rbx = 0s
+    movq $0, %rbx # rbx = 0
     jmp .Loop2Condition
 
 # Now, if there's a 0 in the array, rcx = 1, and rax has the product of the array elements
@@ -31,7 +33,9 @@ product:
 .Loop2:
     movq (%rdi, %rbx, 8), %r8 # r8 = a[i]
     cmpq $0, %r8
-    jz .SetZeroes
+    jz .SetZeroElement
+    cmpq $1, %rcx
+    jz .OneZero
     # ans[i] = Prod / a[i]
     movq %rdx, %r9
     movq $0, %rdx
@@ -42,12 +46,28 @@ product:
     incq %rbx # i++
     jmp .Loop2Condition
 
-.SetZeroes:
-    movq %rax, (%rdx, %rbx, 8)
+.SetZeroElement:
+    movq %rax, (%rdx, %rbx, 8) # a[i] = rax
+    incq %rbx
+    jmp .Loop2Condition
+
+.OneZero:
+    movq $0, (%rdi, %rbx, 8)
     incq %rbx
     jmp .Loop2Condition
 
 .Loop2Condition:
     cmpq %rsi, %rbx # Compare i and n
     jb .Loop2 # Jump to loop if i < n
+    movq $0, %rbx # rbx = 0
+    cmpq $1, %rcx
+    ret
+
+.TwoZeroes:
+    movq $0, (%rdx, %rbx, 8)
+    incq %rbx
+
+.TwoZeroesCondition:
+    cmpq %rsi, %rbx # Compare i and n
+    jb .TwoZeroes # Jump to loop if i < n
     ret
